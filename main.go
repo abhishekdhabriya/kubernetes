ae99b043d0137300fffe46c2e34f281a17f918f2
scriptalert(1)/script <anish2good@yahoo.co.in>
2019-04-12 14:06:22 +0530

    Update main.go

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
-	fmt.Fprintf(w, "Hello, world!\n")
-	fmt.Fprintf(w, "Version: 1.0.0\n")
+	fmt.Fprintf(w, "Kuberenetes by Example!\n")
+	fmt.Fprintf(w, "http://leanpub.com/kube : 1.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
